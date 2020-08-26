f4123c4662b20f97a7b1d4658fa04ccd7823dec8
Sean Tarzy <seantarzy@gmail.com>
2020-08-26 16:45:27 -0400

    got a response for songs

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,3 +1,13 @@
 class Medium < ApplicationRecord
     belongs_to :category
+    include HTTParty
+    def self.get_songs
+        Medium.all.each do |medium| 
+            if medium.url.include?('genius.com')
+                body = HTTParty.get(medium.url).body
+                print body 
+            end
+        end
+    end
+
 end
