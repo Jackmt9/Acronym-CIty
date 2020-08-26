ccd7a288ca43d193231eb43f154dcb4e86e5538a
jackmt9 <jackmt9@gmail.com>
2020-08-25 23:16:51 -0400

    working branch

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,12 +1,6 @@
 class MediaController < ApplicationController
-<<<<<<< HEAD
     def query
         search = params[:query]
         render json: {result: search}
-=======
-    def index
-        media = Medium.all
-        render json: media
->>>>>>> master
     end
 end
