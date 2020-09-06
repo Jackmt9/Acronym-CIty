ef67290a856fcd798130e1056cb75aa41a2c247d
jackmt9 <jackmt9@gmail.com>
2020-09-05 23:09:39 -0400

    getPhrase method started

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -2,11 +2,17 @@ class MediaController < ApplicationController
     def query
         search = params[:query]
         initials = self.getInitials(search)
-        render json: {result: initials}
+        phrase = getPhrase(initials)
+        render json: {result: phrase}
     end
 
     def getInitials(search)
         search.split(' ').map{|w| w[0].upcase}
+    end
+
+    def getPhrase(initials)
+        # insert search logic here
+        intials
     end
 
 end
