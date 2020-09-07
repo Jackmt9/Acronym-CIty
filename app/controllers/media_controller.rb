564cecb9ffdc5cf37e32976dc445f507918bcdf7
Sean Tarzy <seantarzy@gmail.com>
2020-09-07 02:30:40 -0400

    moving stuff to model

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -16,13 +16,34 @@ class MediaController < ApplicationController
     end
 
     def get_phrase(initials)
+        Medium.get_songs(initials)
         # insert search logic here
-            songs = Medium.get_songs.split('/sse')
+            #  song = song.each do |word| 
+            #     if word.include?('[') 
+            #         hit_open = true
+            #         hit_close = false
+            #     end
+            #     if !!hit_open == !!hit_close
+            #         songs_without_nonsense.push(word)
+            #      end
+            #     if word.include?(']')
+            #         hit_close = true
+            #     end
+            # end
+            end
+        # songs_without_nonsense.each do |word|
+        #     if word[0] == word[0].upcase
+        #         new_line = true
+        #     end
+        #     if new_line 
+        #         songs_by_line.push()
+        #     end
+        #     line.push(word)
 
         #   songs =  songs.map do |song|
         #         song.split(']')
         #   end
-          return  songs
+        #   return  songs_without_nonsense
     end
 
 end
