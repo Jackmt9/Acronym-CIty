fa39d26b0971be9464550eb9b20372a9284bef6e
Sean Tarzy <seantarzy@gmail.com>
2020-09-14 16:15:12 -0400

    did the sandwich array

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -29,22 +29,34 @@ class Medium < ApplicationRecord
     end
   end
 
+  def self.split_the_lyrics(range, lyrics)
+    array= []
+    array.push(lyrics[0...range[:index_at]].join(' '))
+    array.push(lyrics[range[:index_at]...range[:end_of_phrase]].join(' '))
+    array.push(lyrics[range[:end_of_phrase]...lyrics.length].join(' '))
+    return array
+  end
+
   def self.query_lyrics(lyrics_array, initials)
     i = 0
     j = 0
-    money_index = 0
+    # money_phrase = ""
     while i < initials.length && j < lyrics_array.length
       if lyrics_array[j][0].downcase == initials[i].downcase
         i += 1
         j += 1
+        # money_phrase += lyrics_array[j] + " "
       else
+        # money_phrase = ""
         i = 0
         j += 1
       end
     end
+    #we need to have a way to return an empty string if it loops through and finds nothing
     money_index = j - initials.length
-    range = { index_at: money_index, length: initials.length }
-    return range
+    range = { index_at: money_index, end_of_phrase: money_index + initials.length }
+
+    return range 
   end
 
   def self.get_songs(initials)
@@ -61,10 +73,13 @@ class Medium < ApplicationRecord
         verse = html.at('.lyrics')
         lyrics = verse.children.text.split(' ')
         range = self.query_lyrics(lyrics, initials)
+        sandwich_array = self.split_the_lyrics(range, lyrics)
         # must insert error handling if no match found
-        range[:lyrics] = lyrics
-        range[:title] = medium.name
-        return range
+        # range[:lyrics] = lyrics
+        # range[:title] = medium.name
+        return {lyrics_array: sandwich_array,
+                song_title: medium.name,
+                artist: medium.author}
       end
     end
     # print phrase_return
